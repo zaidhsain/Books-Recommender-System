c60f5bf346acf171e6eadef4b47a6cf88a4037d8
zaid <zaidhsain2@gmail.com>
2026-01-24 15:39:19 +0100

    chnges added

--- books_recommender/pipeline/training_pipeline.py
+++ books_recommender/pipeline/training_pipeline.py
@@ -1,11 +1,15 @@
 from books_recommender.components.stage_00_data_ingestion import DataIngestion
-
+from books_recommender.components.stage_01_data_validation import DataValidation
+from books_recommender.components.stage_02_data_transformation import DataTransformation
+from books_recommender.components.stage_03_model_trainer import ModelTrainer
 
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
-        
+        self.data_validation = DataValidation()
+        self.data_transformation = DataTransformation()
+        self.model_trainer = ModelTrainer()
 
     def start_training_pipeline(self):
         """
@@ -13,4 +17,6 @@ class TrainingPipeline:
         :return: none
         """
         self.data_ingestion.initiate_data_ingestion()
-      
+        self.data_validation.initiate_data_validation()
+        self.data_transformation.initiate_data_transformation()
+        self.model_trainer.initiate_model_trainer() 
